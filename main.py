37d364d29a58de831b3e78c2af86b84ebdcc62f0
Jeremiah Ford <habib4d@gmail.com>
2024-11-03 10:56:58 -0500

    add files

---
+++ main.py
@@ -0,0 +1,49 @@
+import pygame
+pygame.init()
+screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+clock = pygame.time.Clock()
+running = True
+
+# GLOBAL VARIABLES 
+COLOR = (255, 100, 98) 
+SURFACE_COLOR = (167, 255, 100) 
+WIDTH = 500
+HEIGHT = 500
+
+# Object class 
+class Sprite(pygame.sprite.Sprite): 
+    def __init__(self, color, height, width): 
+        super().__init__() 
+  
+        self.image = pygame.Surface([width, height]) 
+        self.image.fill(SURFACE_COLOR) 
+        self.image.set_colorkey(COLOR) 
+  
+        pygame.draw.rect(self.image, 
+                         color, 
+                         pygame.Rect(0, 0, width, height)) 
+  
+        self.rect = self.image.get_rect()  
+
+while running:
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_q]:
+        running = False
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+
+    # fill the screen with a color to wipe away anything from last frame
+    screen.fill("grey")
+
+    # RENDER YOUR GAME HERE
+
+    # flip() the display to put your work on screen
+    pygame.display.flip()
+
+    clock.tick(60)  # limits FPS to 60
+
+pygame.quit() 
