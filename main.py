75681a29bc0048231d9d8ff410e5398a960be642
Jeremiah Ford <habib4d@gmail.com>
2024-11-03 11:19:39 -0500

    castle class

--- main.py
+++ main.py
@@ -25,36 +25,6 @@ class Sprite(pygame.sprite.Sprite):
   
         self.rect = self.image.get_rect()  
 
-# Castle class
-import pygame
-
-# 1920 x 1080
-castle_health = 1000
-width = 200
-height = 600
-
-class Castle:
-    def __init__(self, cannon, team):
-        self.health = castle_health
-        self.cannon = cannon
-
-        if team:
-            self.x = 1920 - width/2
-        else:
-            self.x = width/2
-        self.y = 1080 - height
-
-    def draw(self):
-        pygame.draw.rect(screen, (0, 0, 0), (self.x, self.y)
-
-
-
-
-
-
-
-
-# game loop
 while running:
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
