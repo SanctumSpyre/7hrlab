caf1ad1f88cae21bcf99a9f4fb9f66ce7cce6266
SanctumSpyre <axe33333@outlook.com>
2024-11-03 16:11:45 -0500

    so i bought a car and it meowed at me.

--- main.py
+++ main.py
@@ -259,7 +259,7 @@ while running:
     for enemy in enemy_ai.troops:
         if enemy.x <= castle_width/2:
             to_delete_enemies.add(enemy)
-            player_castle.health -= enemy.damage*5
+            player_castle.health -= enemy.damage*20
             if player_castle.health <= 0:
                 running = False
         for troop in troops:
