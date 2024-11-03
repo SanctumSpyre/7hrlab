76f4aedf0035203afd68aecf6538ee26391dba1a
SanctumSpyre <axe33333@outlook.com>
2024-11-03 10:48:45 -0500

    e

--- code.py
+++ code.py
@@ -4,6 +4,23 @@ screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
 clock = pygame.time.Clock()
 running = True
 
+
+
+# Object class 
+class Sprite(pygame.sprite.Sprite): 
+    def __init__(self, color, height, width): 
+        super().__init__() 
+  
+        self.image = pygame.Surface([width, height]) 
+        self.image.fill(SURFACE_COLOR) 
+        self.image.set_colorkey(COLOR) 
+  
+        pygame.draw.rect(self.image, 
+                         color, 
+                         pygame.Rect(0, 0, width, height)) 
+  
+        self.rect = self.image.get_rect()  
+        
 while running:
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
